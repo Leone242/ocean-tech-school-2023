df9d1cd142a14b320ddad44a1960168edb5d88cb
Leone <81783433+Leone242@users.noreply.github.com>
2023-09-26 00:07:31 +0000

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Sea of Thieves</h2>
-        <img src="https://nosnerds.com.br/wp-content/uploads/2018/01/seaofthieves-capa.jpg"></img>
-      </div>
+      <Card/>
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import "./Card.css"
+
+export default function Card(){
+    return  <div className="card">
+    <h2>Sea of Thieves</h2>
+    <div className="tags">
+      <spam className="tag">Aventura</spam>
+      <spam className="tag">Multiplayer</spam>
+      <spam className="tag">Mundo Aberto</spam>
+    </div>
+    <img src="https://nosnerds.com.br/wp-content/uploads/2018/01/seaofthieves-capa.jpg"></img>
+  </div>
+}
